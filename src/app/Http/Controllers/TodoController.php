54dbc88eb5723c8b51d54d89c312b33a34c25f1a
金尾太亮 <kanao_taisuke@kinoppusukenoMacBook-Air.local>
2024-12-19 16:37:51 +0900

    add:todo新規作成画面の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -14,4 +14,10 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
+
+    public function create()
+    {
+        return view('todo.create');
+    }
 }
+
